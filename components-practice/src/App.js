969bb78002d777dad2e70c0cc9f4223fea3d9de5
Emilia Zysk <emzysk.dev@gmail.com>
2021-07-08 21:22:18 +0900

    added source to avatar img

--- components-practice/src/App.js
+++ components-practice/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Faker from 'faker';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" />
+          <img alt="avatar" src={faker.image.avatar()}/>
         </a>
         <div className="content">
           <a href="/" className="author">
