a995146c1772b3c98e0342b224ac5f83fe752364
Emilia Zysk <emzysk.dev@gmail.com>
2021-07-09 17:52:25 +0900

    added ApprovalCard.js

--- components-practice/src/App.js
+++ components-practice/src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 // import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Yann"
-        timeAgo="Today at 6:00PM"
-        commentText="Great job!"
-      />
-      <CommentDetail
-        author="Doug"
-        timeAgo="Today at 4:47PM"
-        commentText="Nice one, I liked it"
-      />
-      <CommentDetail
-        author="Trouni"
-        timeAgo="Today at 11:32AM"
-        commentText="Cool cool cool"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Yann"
+          timeAgo="Today at 6:00PM"
+          commentText="Great job!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Doug"
+          timeAgo="Today at 4:47PM"
+          commentText="Nice one, I liked it"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Trouni"
+          timeAgo="Today at 11:32AM"
+          commentText="Cool cool cool"
+        />
+      </ApprovalCard>
     </div>
   );
 };
